1232d00d2467a0597d1953337862d4eeac539191
aschuele <andre.schuele@abas-consulting.com>
2019-06-17 08:46:20 +0200

    ESDKIJP-26: Fixed IntelliJ not loading at all Error

--- src/main/java/icons/Icons.java
+++ src/main/java/icons/Icons.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:411e69bcd27575e31bf2ddb66d23d2873fce842a2a835053129e680c27514e76
-size 1209
+oid sha256:128559e9dfadaba6e5ab5a2fca1b6638579b3b47a83eb94fadd9dbeccd4c4e3e
+size 1201

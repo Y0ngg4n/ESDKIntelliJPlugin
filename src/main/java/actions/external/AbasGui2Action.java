6991aa1abcb20a188194c86f534353c7b5df9756
aschuele <andre.schuele@abas-consulting.com>
2019-06-25 08:58:11 +0200

    ESDKIJP-27: #done Added File Browser to abas and Screen Editor Path #time 1h

---
+++ src/main/java/actions/external/AbasGui2Action.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8b75bb14f18faec3c5e127c290a84c6352d500d691955fe62939df4f5c2eeba
+size 1125
--- src/main/java/actions/settings/PluginSettings.java
+++ src/main/java/actions/settings/PluginSettings.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4715657d7c19a7eeec06ff9edd56bfa18c42978cff742c017171723cd053e14c
-size 3634
+oid sha256:06d8312cb6f0b81afc2e5fda3f3299f20797e39211b8d241c81d1fbe79663dd8
+size 5128

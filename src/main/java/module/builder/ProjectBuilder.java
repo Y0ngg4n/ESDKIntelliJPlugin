a27d5aa2f83f5171c7e5bbd03ab9e43dd269c69b
André Schüle <andre.schuele@abas-consulting.com>
2019-09-17 15:16:53 +0200

    Merge pull request #13 in CONT/esdkintellijplugin from bug/ESDKIJP-35 to master
    Squashed commit of the following:
    
    commit e668bcd8e5f98b75ed3fc358d8fb0ddfd627d132
    Author: aschuele <andre.schuele@abas-consulting.com>
    Date:   Wed Sep 11 11:30:20 2019 +0200
    
        Fixed ESDKIJP-35

--- src/main/java/module/builder/ProjectBuilder.java
+++ src/main/java/module/builder/ProjectBuilder.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab5a9f3e25e5b827aa34d821860de0b97b132d6920a7cb1557c15ecb1c14e193
-size 40288
+oid sha256:8d7bd8e19ca1da4a6abf986c935ac42f8abfb617e51e01fd2e3c91cd8cdc4302
+size 40666
--- src/main/java/module/builder/ProjectReader.java
+++ src/main/java/module/builder/ProjectReader.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a96677ffd6a94e681be8e0a5e9418a285896947569ae0bda5ec4b391ede21597
-size 24214
+oid sha256:50d0cb96cd4c0e56abd06c788b10e2539948efc89c44ee94ecf7258543b896bc
+size 24372
